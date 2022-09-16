0b66550202f377d027e3bb6d83502e7ba1ddc3a6
fluteink <fluteink@qq.com>
2022-09-16 11:41:58 +0800

    通过名字查找

--- mybatis_parameter/src/main/java/com/fluteink/mybatis/mappers/UserMapper.java
+++ mybatis_parameter/src/main/java/com/fluteink/mybatis/mappers/UserMapper.java
@@ -1,8 +1,17 @@
 package com.fluteink.mybatis.mappers;
 
+import com.fluteink.mybatis.pojo.User;
+
 /**
  * @author 明宇
  * @version 1.0
  */
 public interface UserMapper {
+    /**
+     * 根据用户名查询用户信息
+     *
+     * @param username
+     * @return
+     */
+    User getUserByUsername(String username);
 }
---
+++ mybatis_parameter/src/test/java/com/fluteink/mybatis/test/parameter.java
@@ -0,0 +1,21 @@
+package com.fluteink.mybatis.test;
+
+import com.fluteink.mybatis.mappers.UserMapper;
+import com.fluteink.mybatis.pojo.User;
+import com.fluteink.mybatis.utils.SqlSessionUtil;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+
+/**
+ * @author 明宇
+ * @version 1.0
+ */
+public class parameter {
+    @Test
+    public void testgetUserByUsername() {
+        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        User admin = mapper.getUserByUsername("admin");
+        System.out.println(admin);
+    }
+}
