a083085d3f5c8a1c89cca9eef351b64752a15f46
fluteink <fluteink@qq.com>
2022-09-19 21:17:19 +0800

    提交

--- mybatis_parameter/src/main/java/com/fluteink/mybatis/mappers/SelectMapper.java
+++ mybatis_parameter/src/main/java/com/fluteink/mybatis/mappers/SelectMapper.java
@@ -1,12 +1,22 @@
 package com.fluteink.mybatis.mappers;
 
 import com.fluteink.mybatis.pojo.User;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 /**
  * @author 明宇
  * @version 1.0
  */
 public interface SelectMapper {
+    /**
+     * 根据id查用户
+     *
+     * @param id
+     * @return
+     */
+    User getUserById(@Param("id") Integer id);
 
-    User getUserById()
+    List<User> getAllUsers();
 }
--- mybatis_parameter/src/test/java/com/fluteink/mybatis/test/SelectMapper.java
+++ mybatis_parameter/src/test/java/com/fluteink/mybatis/test/SelectMapper.java
@@ -1,8 +1,31 @@
 package com.fluteink.mybatis.test;
 
+import com.fluteink.mybatis.pojo.User;
+import com.fluteink.mybatis.utils.SqlSessionUtil;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+
+import java.util.List;
+
 /**
  * @author 明宇
  * @version 1.0
  */
 public class SelectMapper {
+    @Test
+    public void testGetUserById() {
+        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+        com.fluteink.mybatis.mappers.SelectMapper mapper = sqlSession.getMapper(com.fluteink.mybatis.mappers.SelectMapper.class);
+        User userById = mapper.getUserById(1);
+        System.out.println(userById);
+    }
+
+    @Test
+    public void getAllUsers() {
+        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+        com.fluteink.mybatis.mappers.SelectMapper mapper = sqlSession.getMapper(com.fluteink.mybatis.mappers.SelectMapper.class);
+        List<User> allUsers = mapper.getAllUsers();
+        allUsers.forEach(System.out::println);
+
+    }
 }
