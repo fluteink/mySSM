4e9755060be304ee43da3a74e55ceb847ba9ac3b
fluteink <fluteink@qq.com>
2022-09-19 21:01:38 +0800

    提交

--- mybatis_parameter/src/main/java/com/fluteink/mybatis/mappers/SelectMapper.java
+++ mybatis_parameter/src/main/java/com/fluteink/mybatis/mappers/SelectMapper.java
@@ -1,8 +1,12 @@
 package com.fluteink.mybatis.mappers;
 
+import com.fluteink.mybatis.pojo.User;
+
 /**
  * @author 明宇
  * @version 1.0
  */
 public interface SelectMapper {
+
+    User getUserById()
 }
