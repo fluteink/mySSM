e18a6990cb8213cbf7815d99bac8923aaa9f559a
fluteink <fluteink@qq.com>
2022-09-20 17:36:40 +0800

    提交

--- mybatis_resultMap/src/main/java/com/fluteink/mybatis/mappers/EmpMapper.java
+++ mybatis_resultMap/src/main/java/com/fluteink/mybatis/mappers/EmpMapper.java
@@ -1,11 +1,12 @@
 package com.fluteink.mybatis.mappers;
 
 import com.fluteink.mybatis.pojo.Emp;
+import org.apache.ibatis.annotations.Param;
 
 /**
  * @author 明宇
  * @version 1.0
  */
 public interface EmpMapper {
-    Emp getEmpByEmpId
+    Emp getEmpByEmpId (@Param("empId") Integer empId);
 }
