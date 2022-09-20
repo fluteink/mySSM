5fc75c0ab98a329dc0b1eb100b8b5bd43c295fb5
fluteink <fluteink@qq.com>
2022-09-20 17:42:53 +0800

    提交

---
+++ mybatis_resultMap/src/test/java/ResultMapTest.java
@@ -0,0 +1,19 @@
+import com.fluteink.mybatis.mappers.EmpMapper;
+import com.fluteink.mybatis.pojo.Emp;
+import com.fluteink.mybatis.utils.SqlSessionUtil;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+
+/**
+ * @author 明宇
+ * @version 1.0
+ */
+public class ResultMapTest {
+    @Test
+    public void testGetEmpByEmpId() {
+        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+        EmpMapper mapper = sqlSession.getMapper(EmpMapper.class);
+        Emp empByEmpId = mapper.getEmpByEmpId(1);
+        System.out.println(empByEmpId);
+    }
+}
