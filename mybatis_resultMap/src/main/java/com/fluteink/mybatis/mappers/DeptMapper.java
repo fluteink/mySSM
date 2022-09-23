ae9aa002625797ef506bb60d6dc3e42418d173bf
fluteink <fluteink@qq.com>
2022-09-23 10:22:42 +0800

    提交

--- mybatis_resultMap/src/main/java/com/fluteink/mybatis/mappers/DeptMapper.java
+++ mybatis_resultMap/src/main/java/com/fluteink/mybatis/mappers/DeptMapper.java
@@ -1,8 +1,12 @@
 package com.fluteink.mybatis.mappers;
 
+import com.fluteink.mybatis.pojo.Dept;
+import org.apache.ibatis.annotations.Param;
+
 /**
  * @author 明宇
  * @version 1.0
  */
 public interface DeptMapper {
+    Dept getEmpAndDeptByStep2(@Param("deptId") Integer deptId);
 }
--- mybatis_resultMap/src/test/java/ResultMapTest.java
+++ mybatis_resultMap/src/test/java/ResultMapTest.java
@@ -1,6 +1,9 @@
+import com.fluteink.mybatis.mappers.DeptMapper;
 import com.fluteink.mybatis.mappers.EmpMapper;
+import com.fluteink.mybatis.pojo.Dept;
 import com.fluteink.mybatis.pojo.Emp;
 import com.fluteink.mybatis.utils.SqlSessionUtil;
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 
@@ -22,5 +25,12 @@ public class ResultMapTest {
         EmpMapper mapper = sqlSession.getMapper(EmpMapper.class);
         Emp empAndDeptByEmpId = mapper.getEmpAndDeptByEmpId(1);
         System.out.println(empAndDeptByEmpId);
+    }
+    @Test
+    public void getDept() {
+        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+        DeptMapper mapper = sqlSession.getMapper(DeptMapper.class);
+        Dept empAndDeptByStep2 = mapper.getEmpAndDeptByStep2(1);
+        System.out.println(empAndDeptByStep2);
     }
 }
