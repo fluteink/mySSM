2769b9073fef9d936b09a0526d617049c846514c
fluteink <fluteink@qq.com>
2022-09-26 11:51:03 +0800

    提交

--- mybatis_cache/src/test/java/com/fluteink/mybatis/test/CacheMapperTest.java
+++ mybatis_cache/src/test/java/com/fluteink/mybatis/test/CacheMapperTest.java
@@ -6,17 +6,20 @@ import com.fluteink.mybatis.utils.SqlSessionUtil;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 
+import java.util.Scanner;
+
 /**
  * @author 明宇
  * @version 1.0
  */
 public class CacheMapperTest {
     @Test
-    public void testGetEmpByEmpId() {
+    public void testGetEmpByEmpId() throws InterruptedException {
         SqlSession sqlSession = SqlSessionUtil.getSqlSession();
         CacheMapper mapper = sqlSession.getMapper(CacheMapper.class);
         Emp emp = mapper.getEmpByEmpId(1);
         System.out.println(emp);
+//        Thread.sleep(10000);
         Emp emp2 = mapper.getEmpByEmpId(1);
         System.out.println(emp2);
         SqlSession sqlSession2 = SqlSessionUtil.getSqlSession();
