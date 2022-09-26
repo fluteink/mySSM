533eb47f7aca6c4166d9a69828e40097a78de0cc
fluteink <fluteink@qq.com>
2022-09-26 11:23:38 +0800

    提交

--- mybatis_cache/src/main/java/com/fluteink/mybatis/mappers/CacheMapper.java
+++ mybatis_cache/src/main/java/com/fluteink/mybatis/mappers/CacheMapper.java
@@ -1,8 +1,12 @@
 package com.fluteink.mybatis.mappers;
 
+import com.fluteink.mybatis.pojo.Emp;
+import org.apache.ibatis.annotations.Param;
+
 /**
  * @author 明宇
  * @version 1.0
  */
 public interface CacheMapper {
+    Emp getEmpByEmpId(@Param("empId")Integer empId);
 }
