c9f85e5cc83a917e1c5be007be1a36c8dee0b0ae
fluteink <fluteink@qq.com>
2022-09-26 11:27:50 +0800

    提交

---
+++ mybatis_cache/src/test/java/com/fluteink/mybatis/test/CacheMapperTest.java
@@ -0,0 +1,21 @@
+package com.fluteink.mybatis.test;
+
+import com.fluteink.mybatis.mappers.CacheMapper;
+import com.fluteink.mybatis.pojo.Emp;
+import com.fluteink.mybatis.utils.SqlSessionUtil;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+
+/**
+ * @author 明宇
+ * @version 1.0
+ */
+public class CacheMapperTest {
+    @Test
+    public void testGetEmpByEmpId() {
+        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+        CacheMapper mapper = sqlSession.getMapper(CacheMapper.class);
+        Emp emp = mapper.getEmpByEmpId(1);
+        System.out.println(emp);
+    }
+}
