aa5e25baeeb502abb642e502f629a2a4d03543f2
fluteink <fluteink@qq.com>
2022-09-28 22:16:13 +0800

    提交

--- mybatis_mbg/src/test/java/com/fluteink/mybatis/test/MBGTest.java
+++ mybatis_mbg/src/test/java/com/fluteink/mybatis/test/MBGTest.java
@@ -2,9 +2,12 @@ package com.fluteink.mybatis.test;
 
 import com.fluteink.mybatis.mapper.EmpMapper;
 import com.fluteink.mybatis.pojo.Emp;
+import com.fluteink.mybatis.pojo.EmpExample;
 import com.fluteink.mybatis.utils.SqlSessionUtil;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
+
+import java.util.List;
 
 /**
  * @author 明宇
@@ -15,7 +18,11 @@ public class MBGTest {
     public void testMBG() {
         SqlSession sqlSession = SqlSessionUtil.getSqlSession();
         EmpMapper mapper = sqlSession.getMapper(EmpMapper.class);
-        Emp emp = mapper.selectByPrimaryKey(1);
-        System.out.println(emp);
+//        Emp emp = mapper.selectByPrimaryKey(1);
+//        System.out.println(emp);
+//        List<Emp> emps = mapper.selectByExample(null);
+//        System.out.println(emps);
+        EmpExample empExample = new EmpExample();
+
     }
 }
