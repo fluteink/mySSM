27e453122a1e4b0d8f59ce0ec7c7c422b76b2511
fluteink <fluteink@qq.com>
2022-09-29 22:02:39 +0800

    commit

---
+++ spring_helloworld/src/main/java/com/fluteink/spring/pojo/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.fluteink.spring.pojo;
+
+/**
+ * @author 明宇
+ * @version 1.0
+ */
+public class HelloWorld {
+    public void sayHello() {
+        System.out.println("hello,spring");
+    }
+}
