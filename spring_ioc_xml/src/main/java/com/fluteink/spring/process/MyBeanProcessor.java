21b26d3c645bb30519104a9b23104532ef88bdbf
fluteink <fluteink@qq.com>
2022-10-04 21:54:26 +0800

    commit

---
+++ spring_ioc_xml/src/main/java/com/fluteink/spring/process/MyBeanProcessor.java
@@ -0,0 +1,22 @@
+package com.fluteink.spring.process;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
+/**
+ * @author 明宇
+ * @version 1.0
+ */
+public class MyBeanProcessor implements BeanPostProcessor {
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("postProcessBeforeInitialization");
+        return BeanPostProcessor.super.postProcessBeforeInitialization(bean, beanName);
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("postProcessAfterInitialization");
+        return BeanPostProcessor.super.postProcessAfterInitialization(bean, beanName);
+    }
+}
