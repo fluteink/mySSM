9bf5d364a636cf40ae345a30b304358c499550b9
fluteink <fluteink@qq.com>
2022-10-04 18:49:45 +0800

    commit

---
+++ spring_ioc_xml/src/test/java/com/fluteink/spring/test/ScopeTest.java
@@ -0,0 +1,20 @@
+package com.fluteink.spring.test;
+
+import com.fluteink.spring.pojo.Student;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author 明宇
+ * @version 1.0
+ */
+public class ScopeTest {
+    @Test
+    public void testScope(){
+        ApplicationContext ioc = new ClassPathXmlApplicationContext("spring-scope.xml");
+        Student student1 = ioc.getBean(Student.class);
+        Student student2 = ioc.getBean(Student.class);
+        System.out.println(student1 == student2);
+    }
+}
