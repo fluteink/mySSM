d725dbd9739ed60187711a679b76b3c1acf7cb13
fluteink <fluteink@qq.com>
2022-10-09 14:19:35 +0800

    commit

--- spring_ioc_annotation/src/test/java/com/fluteink/spring/test/IOCByAnnotationTest.java
+++ spring_ioc_annotation/src/test/java/com/fluteink/spring/test/IOCByAnnotationTest.java
@@ -1,6 +1,11 @@
 package com.fluteink.spring.test;
 
+import com.fluteink.spring.controller.UserController;
+import com.fluteink.spring.dao.UserDao;
+import com.fluteink.spring.service.UserService;
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * @author 明宇
@@ -9,6 +14,12 @@ import org.junit.Test;
 public class IOCByAnnotationTest {
     @Test
     public void test() {
-
+        ApplicationContext ioc = new ClassPathXmlApplicationContext("spring-ioc-annotation.xml");
+        UserController bean = ioc.getBean(UserController.class);
+        System.out.println(bean);
+        UserService bean1 = ioc.getBean(UserService.class);
+        System.out.println(bean1);
+        UserDao bean2 = ioc.getBean(UserDao.class);
+        System.out.println(bean2);
     }
 }
