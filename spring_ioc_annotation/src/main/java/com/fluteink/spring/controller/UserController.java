3c62e008e5fec064fa110da1e34c04f507f61e6a
fluteink <fluteink@qq.com>
2022-10-09 13:31:09 +0800

    commit

---
+++ spring_ioc_annotation/src/main/java/com/fluteink/spring/controller/UserController.java
@@ -0,0 +1,8 @@
+package com.fluteink.spring.controller;
+
+/**
+ * @author 明宇
+ * @version 1.0
+ */
+public class UserController {
+}
---
+++ spring_ioc_annotation/src/main/java/com/fluteink/spring/dao/UserDao.java
@@ -0,0 +1,8 @@
+package com.fluteink.spring.dao;
+
+/**
+ * @author 明宇
+ * @version 1.0
+ */
+public interface UserDao {
+}
