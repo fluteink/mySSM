f1112df2719e931c45f8a4d316297604a4b3b8cf
fluteink <fluteink@qq.com>
2022-10-11 15:23:47 +0800

    commit

--- spring-aop/src/main/java/com/fluteink/spring/aop/annptation/LoggerAspect.java
+++ spring-aop/src/main/java/com/fluteink/spring/aop/annptation/LoggerAspect.java
@@ -1,5 +1,7 @@
 package com.fluteink.spring.aop.annptation;
 
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 /**
@@ -7,5 +9,10 @@ import org.springframework.stereotype.Component;
  * @version 1.0
  */
 @Component
+@Aspect
 public class LoggerAspect {
+    @Before("execution(public int com.fluteink.spring.aop.annptation.CalculatorImpl.add(int, int))")
+    public void BeforeAdviceMethod() {
+        System.out.println("LoggerAspect,前置通知");
+    }
 }
