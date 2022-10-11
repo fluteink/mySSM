c2b620572769cf05f888454c80b290e73a456761
fluteink <fluteink@qq.com>
2022-10-11 18:48:07 +0800

    commit

---
+++ spring-transaction/src/test/java/JdbcTemplateTest.java
@@ -0,0 +1,23 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @author 明宇
+ * @version 1.0
+ */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:spring-jdbctemplate.xml ")
+public class JdbcTemplateTest {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+    @Test
+    public void testInsert() {
+        String sql="insert into t_user values(null,?,?,?,?,?)";
+        jdbcTemplate.update(sql,"root","123",23,"女","123@qq.com");
+    }
+}
