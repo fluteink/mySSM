3dcaf379ef346463aa06f54598187dc76e6d9046
fluteink <fluteink@qq.com>
2022-10-11 19:00:27 +0800

    commit

--- spring-transaction/src/test/java/JdbcTemplateTest.java
+++ spring-transaction/src/test/java/JdbcTemplateTest.java
@@ -1,9 +1,13 @@
+import com.fluteink.spring.pojo.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
 
 /**
  * @author 明宇
@@ -21,8 +25,18 @@ public class JdbcTemplateTest {
         String sql = "insert into t_user values(null,?,?,?,?,?)";
         jdbcTemplate.update(sql, "root", "123", 23, "女", "123@qq.com");
     }
+
     @Test
     public void testGetUserById() {
+        String sql = "select * from t_user where id = ?";
+        User user = jdbcTemplate.queryForObject(sql, new BeanPropertyRowMapper<>(User.class), 1);
+        System.out.println(user);
+    }
+    @Test
+    public void testGetAllUsers() {
+        String sql="select * from t_user";
+        List<User> users = jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(User.class));
+        users.forEach(System.out::println);
 
     }
 }
