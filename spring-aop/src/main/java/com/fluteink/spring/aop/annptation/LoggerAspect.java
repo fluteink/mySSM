68e3336bac37a47de85f445680fd39df8c961f4c
fluteink <fluteink@qq.com>
2022-10-11 15:45:53 +0800

    commit

--- spring-aop/src/main/java/com/fluteink/spring/aop/annptation/LoggerAspect.java
+++ spring-aop/src/main/java/com/fluteink/spring/aop/annptation/LoggerAspect.java
@@ -1,8 +1,12 @@
 package com.fluteink.spring.aop.annptation;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.Signature;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
 
 /**
  * @author 明宇
@@ -13,8 +17,10 @@ import org.springframework.stereotype.Component;
 public class LoggerAspect {
 //    @Before("execution(public int com.fluteink.spring.aop.annptation.CalculatorImpl.add(int, int))")
     @Before("execution(* com.fluteink.spring.aop.annptation.CalculatorImpl.*(..))")
-    public void BeforeAdviceMethod() {
-        System.out.println("LoggerAspect,前置通知");
+    public void BeforeAdviceMethod(JoinPoint joinPoint) {
+        Signature signature = joinPoint.getSignature();
+        Object[] args = joinPoint.getArgs();
+        System.out.println("LoggerAspect,前置通知,方法"+signature.getName()+"参数"+ Arrays.toString(args));
     }
 
 }
