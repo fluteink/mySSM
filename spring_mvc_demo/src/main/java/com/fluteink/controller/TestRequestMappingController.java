48d0c54dab764877374d7fd493c8a64895986f88
fluteink <fluteink@qq.com>
2022-10-12 21:45:36 +0800

    commit

--- spring_mvc_demo/src/main/java/com/fluteink/controller/TestRequestMappingController.java
+++ spring_mvc_demo/src/main/java/com/fluteink/controller/TestRequestMappingController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @version 1.0
  */
 @Controller
-@RequestMapping("/test")
+//@RequestMapping("/test")
 public class TestRequestMappingController {
 
     @RequestMapping("/hello")
