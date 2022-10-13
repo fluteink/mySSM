75116b32f302446bfcbea3749f962bfed543e1fe
fluteink <fluteink@qq.com>
2022-10-13 14:59:16 +0800

    commit

--- spring_mvc_demo/src/main/java/com/fluteink/controller/TestRequestMappingController.java
+++ spring_mvc_demo/src/main/java/com/fluteink/controller/TestRequestMappingController.java
@@ -27,4 +27,8 @@ public class TestRequestMappingController {
     public String testAnt(){
         return "success";
     }
+    @RequestMapping("/test/rest/1")
+    public String testRest(){
+        return "success";
+    }
 }
