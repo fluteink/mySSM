d1b6907dd1a02ef5c404d90276d44b6870937014
fluteink <fluteink@qq.com>
2022-10-14 16:25:27 +0800

    Revert "commit"
    This reverts commit 05c9a1d9ea117dbb5b5a68d9c02841b805aa789a.

--- spring_mvc_demo/src/main/java/com/fluteink/controller/TestParamController.java
+++ spring_mvc_demo/src/main/java/com/fluteink/controller/TestParamController.java
@@ -1,14 +1,11 @@
 package com.fluteink.controller;
 
-import com.fluteink.pojo.User;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
 /**
  * @author 明宇
@@ -18,7 +15,6 @@ import javax.servlet.http.HttpSession;
 public class TestParamController {
     @RequestMapping("/param/servletAPI")
     public String getParamByServletAPI(HttpServletRequest request) {
-        HttpSession session = request.getSession();
         String username = request.getParameter("username");
         String password = request.getParameter("password");
         System.out.println("username:" + username + " password:" + password);
@@ -26,16 +22,9 @@ public class TestParamController {
     }
 
     @RequestMapping("/param")
-    public String getParam(@RequestParam(value = "userName", required = true, defaultValue = "hello") String username,
-                           String password, @RequestHeader("referer") String referer,
-                           @CookieValue("JSESSIONID") String jsessionId) {
-        System.out.println("jsessionId" + jsessionId);
-        System.out.println("username:" + username + " password:" + password + "   referer:" + referer);
-        return "success";
-    }
-    @RequestMapping("/param/pojo")
-    public String getParamByPojo(User user){
-        System.out.println(user);
+    public String getParam(@RequestParam(value = "userName",required = true ,defaultValue = "hello")  String username,
+                           String password, @RequestHeader("referer") String referer) {
+        System.out.println("username:" + username + " password:" + password+"   referer:"+referer);
         return "success";
     }
 }
