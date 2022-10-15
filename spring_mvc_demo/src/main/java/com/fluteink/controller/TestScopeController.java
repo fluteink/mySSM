cfe6f9f8afb6a2c6d5a1dab83fedbcd81040ee66
fluteink <fluteink@qq.com>
2022-10-15 17:13:05 +0800

    commit

---
+++ spring_mvc_demo/src/main/java/com/fluteink/controller/TestScopeController.java
@@ -0,0 +1,20 @@
+package com.fluteink.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @author 明宇
+ * @version 1.0
+ */
+@Controller
+public class TestScopeController {
+    @RequestMapping("/test/mav")
+    public ModelAndView testMAV() {
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("testRequestScope","hello,ModelAndView");
+
+        return mav;
+    }
+}
