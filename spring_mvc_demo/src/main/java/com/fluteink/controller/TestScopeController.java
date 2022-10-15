047167b823310d8318990891e2a5a52eca39e4b8
fluteink <fluteink@qq.com>
2022-10-15 17:28:00 +0800

    commit

--- spring_mvc_demo/src/main/java/com/fluteink/controller/TestScopeController.java
+++ spring_mvc_demo/src/main/java/com/fluteink/controller/TestScopeController.java
@@ -1,6 +1,8 @@
 package com.fluteink.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -16,5 +18,15 @@ public class TestScopeController {
         mav.addObject("testRequestScope", "hello,ModelAndView");
         mav.setViewName("success");
         return mav;
+    }
+    @RequestMapping("/test/model")
+    public String testModel(Model model) {
+        model.addAttribute("testRequestScope","hello,model!");
+        return "success";
+    }
+    @RequestMapping("/test/map")
+    public String testMap(ModelMap modelMap) {
+        modelMap.addAttribute("testRequestScope","hello,modelMap!");
+        return "success";
     }
 }
