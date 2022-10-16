3e8fe07522e3fced2675c9f264960bc69cf06701
fluteink <fluteink@qq.com>
2022-10-16 17:43:44 +0800

    commit

---
+++ spring_mvc_demo/src/main/java/com/fluteink/controller/TestViewController.java
@@ -0,0 +1,21 @@
+package com.fluteink.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author 明宇
+ * @version 1.0
+ */
+@Controller
+public class TestViewController {
+    @RequestMapping("/test/view/thymeleaf")
+    public String testThymeleafView(){
+        return "success";
+    }
+    @RequestMapping("/test/view/forward")
+    public String testInternalResourceView(){
+        return "forward:/test/model";
+    }
+
+}
