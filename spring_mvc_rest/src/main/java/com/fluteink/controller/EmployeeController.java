50fb27624a99961428c858df0fbb0f3cc5dd86d3
fluteink <fluteink@qq.com>
2022-10-17 20:22:06 +0800

    commit

---
+++ spring_mvc_rest/src/main/java/com/fluteink/controller/EmployeeController.java
@@ -0,0 +1,12 @@
+package com.fluteink.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author 明宇
+ * @version 1.0
+ */
+@Controller
+public class EmployeeController {
+
+}
