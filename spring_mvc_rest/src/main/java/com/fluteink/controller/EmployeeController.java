e45a9a8175c637222be370a84339ce5124b776eb
fluteink <fluteink@qq.com>
2022-10-17 21:07:35 +0800

    commit

--- spring_mvc_rest/src/main/java/com/fluteink/controller/EmployeeController.java
+++ spring_mvc_rest/src/main/java/com/fluteink/controller/EmployeeController.java
@@ -1,8 +1,14 @@
 package com.fluteink.controller;
 
 import com.fluteink.dao.EmployeeDao;
+import com.fluteink.pojo.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Collection;
 
 /**
  * @author 明宇
@@ -13,4 +19,10 @@ public class EmployeeController {
     @Autowired
     private EmployeeDao employeeDao;
 
+    @RequestMapping(value = "/employee", method = RequestMethod.GET)
+    public String getAllEmployees(Model model) {
+        Collection<Employee> allEmployees = employeeDao.getAll();
+        model.addAttribute("AllEmployees",allEmployees);
+        return "employee_list";
+    }
 }
