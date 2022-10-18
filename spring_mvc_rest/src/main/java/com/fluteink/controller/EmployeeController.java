6b5d2563f39e3169c9bbfde73190f4ec6141453d
fluteink <fluteink@qq.com>
2022-10-18 20:12:22 +0800

    commit

--- spring_mvc_rest/src/main/java/com/fluteink/controller/EmployeeController.java
+++ spring_mvc_rest/src/main/java/com/fluteink/controller/EmployeeController.java
@@ -5,6 +5,7 @@ import com.fluteink.pojo.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -27,6 +28,17 @@ public class EmployeeController {
     }
     @RequestMapping(value = "/employee",method = RequestMethod.POST)
     public String addEmployee(Employee employee){
+        employeeDao.save(employee);
+        return "redirect:/employee";
+    }
+    @RequestMapping(value="/employee/{id}",method = RequestMethod.GET)
+    public String toUpdate(@PathVariable("id") Integer id,Model model){
+        Employee employee = employeeDao.get(id);
+        model.addAttribute("employee",employee);
+        return "employee_update";
+    }
+    @RequestMapping(value="/employee",method = RequestMethod.PUT)
+    public  String updateEmployee(Employee employee){
         employeeDao.save(employee);
         return "redirect:/employee";
     }
