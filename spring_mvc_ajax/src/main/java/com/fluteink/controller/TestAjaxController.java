41b6a3f746e04a173cd6022b8a385a5f8dd75272
fluteink <fluteink@qq.com>
2022-10-19 17:52:44 +0800

    commit

--- spring_mvc_ajax/src/main/java/com/fluteink/controller/TestAjaxController.java
+++ spring_mvc_ajax/src/main/java/com/fluteink/controller/TestAjaxController.java
@@ -1,6 +1,10 @@
 package com.fluteink.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * @author 明宇
@@ -8,5 +12,9 @@ import org.springframework.stereotype.Controller;
  */
 @Controller
 public class TestAjaxController {
-
+@RequestMapping("/test/ajax")
+    public void testAjax(Integer id, HttpServletResponse response) throws IOException {
+    System.out.println("id"+id);
+    response.getWriter().write("hello,axios!!");
+}
 }
