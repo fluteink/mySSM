440b642ed08850386c80f311acaef777d623441e
fluteink <fluteink@qq.com>
2022-10-19 22:31:45 +0800

    commit

--- spring_mvc_ajax/src/main/java/com/fluteink/controller/TestAjaxController.java
+++ spring_mvc_ajax/src/main/java/com/fluteink/controller/TestAjaxController.java
@@ -1,11 +1,13 @@
 package com.fluteink.controller;
 
+import com.fluteink.pojo.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
 
 /**
  * @author 明宇
@@ -20,8 +22,14 @@ public class TestAjaxController {
         response.getWriter().write("hello,axios!!");
     }
 
-    @RequestMapping("test/RequestBody/json")
-    public void testRequestBody(HttpServletResponse response) throws IOException {
+//    @RequestMapping("/test/RequestBody/json")
+    public void testRequestBody(@RequestBody User user, HttpServletResponse response) throws IOException {
+        System.out.println(user);
         response.getWriter().write("hello,RequestBody!!");
+    }
+    @RequestMapping("/test/RequestBody/json")
+    public void testRequestBody(@RequestBody Map<String,Object> map, HttpServletResponse response) throws IOException {
+        System.out.println(map);
+        response.getWriter().write("hello,RequestBody!!!!!!!");
     }
 }
