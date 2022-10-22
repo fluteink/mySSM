4c970cd67538d3307f75b49524b554afbe6df207
fluteink <fluteink@qq.com>
2022-10-22 17:12:01 +0800

    commit

--- spring_mvc_annotation/src/main/java/com/fluteink/config/WebInit.java
+++ spring_mvc_annotation/src/main/java/com/fluteink/config/WebInit.java
@@ -1,6 +1,10 @@
 package com.fluteink.config;
 
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.Filter;
 
 /**
  * @author 明宇
@@ -21,5 +25,14 @@ public class WebInit extends AbstractAnnotationConfigDispatcherServletInitialize
     @Override
     protected String[] getServletMappings() {
         return new String[]{"/"};
+    }
+
+    @Override
+    protected Filter[] getServletFilters() {
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("UTF-8");
+        characterEncodingFilter.setForceEncoding(true);
+        HiddenHttpMethodFilter hiddenHttpMethodFilter = new HiddenHttpMethodFilter();
+        return new Filter[]{characterEncodingFilter,hiddenHttpMethodFilter};
     }
 }
