fb8a91225179993895e271c9b6cbebc2397dd8c3
fluteink <fluteink@qq.com>
2022-10-22 15:55:22 +0800

    commit

--- spring_mvc_ajax/src/main/java/com/fluteink/controller/ExceptionController.java
+++ spring_mvc_ajax/src/main/java/com/fluteink/controller/ExceptionController.java
@@ -1,6 +1,8 @@
 package com.fluteink.controller;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 /**
  * @author 明宇
@@ -9,4 +11,9 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 @ControllerAdvice
 public class ExceptionController {
 
+    @ExceptionHandler(ArithmeticException.class)
+    public String handleException(Throwable ex,Model model){
+        model.addAttribute("ex",ex);
+        return "error";
+    }
 }
