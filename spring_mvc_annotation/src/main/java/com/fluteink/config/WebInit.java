4d8dd64f41d01e7294de25014a2a398ae5047515
fluteink <fluteink@qq.com>
2022-10-22 16:58:16 +0800

    commit

--- spring_mvc_annotation/src/main/java/com/fluteink/config/WebInit.java
+++ spring_mvc_annotation/src/main/java/com/fluteink/config/WebInit.java
@@ -1,9 +1,25 @@
 package com.fluteink.config;
 
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
 /**
  * @author 明宇
  * @version 1.0
  */
-public class WebInit {
+public class WebInit extends AbstractAnnotationConfigDispatcherServletInitializer {
 
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[0];
+    }
 }
