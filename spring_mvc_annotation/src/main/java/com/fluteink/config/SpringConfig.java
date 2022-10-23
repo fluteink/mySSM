2a5e5ba3a0269633201cf64d277643c45b725dbe
fluteink <fluteink@qq.com>
2022-10-23 10:49:14 +0800

    commit

--- spring_mvc_annotation/src/main/java/com/fluteink/config/SpringConfig.java
+++ spring_mvc_annotation/src/main/java/com/fluteink/config/SpringConfig.java
@@ -1,9 +1,12 @@
 package com.fluteink.config;
 
+import org.springframework.context.annotation.Configuration;
+
 /**
  * @author 明宇
  * @version 1.0
  */
+@Configuration
 public class SpringConfig {
 
 }
--- spring_mvc_annotation/src/main/java/com/fluteink/config/WebConfig.java
+++ spring_mvc_annotation/src/main/java/com/fluteink/config/WebConfig.java
@@ -1,9 +1,16 @@
 package com.fluteink.config;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 /**
  * @author 明宇
  * @version 1.0
  */
-public class WebConfig {
+@Configuration
+//扫描组件
+@ComponentScan("com.fluteink.controller")
+public class WebConfig implements WebMvcConfigurer {
 
 }
