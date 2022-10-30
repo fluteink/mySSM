5085433f863fa49840a0c0b1f75b2cc759340e49
fluteink <fluteink@qq.com>
2022-10-30 20:51:28 +0800

    commit

--- ssm/src/main/java/com/fluteink/ssm/mapper/EmployeeMapper.java
+++ ssm/src/main/java/com/fluteink/ssm/mapper/EmployeeMapper.java
@@ -1,6 +1,9 @@
-package com.fluteink.ssm.mapper;/**
- * @author  明宇
+package com.fluteink.ssm.mapper;
+
+/**
+ * @author 明宇
  * @version 1.0
- */public class EmployeeMapper {
-    
+ */
+public interface EmployeeMapper {
+
 }
