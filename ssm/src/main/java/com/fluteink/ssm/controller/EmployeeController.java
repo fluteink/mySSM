f93a91697e60fe78666ef7e7a0cc1fd6805bdfb4
fluteink <fluteink@qq.com>
2022-10-30 21:55:52 +0800

    提交

--- ssm/src/main/java/com/fluteink/ssm/controller/EmployeeController.java
+++ ssm/src/main/java/com/fluteink/ssm/controller/EmployeeController.java
@@ -1,6 +1,14 @@
 package com.fluteink.ssm.controller;
 
+import com.fluteink.ssm.pojo.Employee;
+import com.fluteink.ssm.service.impl.EmployeeServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
 
 /**
  * @author 明宇
@@ -8,5 +16,12 @@ import org.springframework.stereotype.Controller;
  */
 @Controller
 public class EmployeeController {
-
+    @Autowired
+    private EmployeeServiceImpl employeeService;
+    @RequestMapping(value = "employee",method = RequestMethod.GET)
+    public String getAllEmployees(Model model) {
+    List<Employee> list= employeeService.getAllEmployees();
+    model.addAttribute("list",list);
+    return "employee_list";
+}
 }
--- ssm/src/main/java/com/fluteink/ssm/mapper/EmployeeMapper.java
+++ ssm/src/main/java/com/fluteink/ssm/mapper/EmployeeMapper.java
@@ -1,9 +1,14 @@
 package com.fluteink.ssm.mapper;
 
+import com.fluteink.ssm.pojo.Employee;
+
+import java.util.List;
+
 /**
  * @author 明宇
  * @version 1.0
  */
 public interface EmployeeMapper {
 
+    List<Employee> getAllEmployees();
 }
