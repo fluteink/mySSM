2b2b51e9e6ad7e9eab0ec540659d1f26c8057069
fluteink <fluteink@qq.com>
2022-10-30 21:10:46 +0800

    提交

--- ssm/src/main/java/com/fluteink/ssm/service/impl/EmployeeServiceImpl.java
+++ ssm/src/main/java/com/fluteink/ssm/service/impl/EmployeeServiceImpl.java
@@ -1,6 +1,8 @@
 package com.fluteink.ssm.service.impl;
 
 import com.fluteink.ssm.service.EmployeeService;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
