cf526d99cf4c6dc56fa00aeb265b4b74174f0462
fluteink <fluteink@qq.com>
2022-10-30 17:41:37 +0800

    commit

---
+++ ssm/src/main/java/com/fluteink/ssm/service/EmployeeService.java
@@ -0,0 +1,8 @@
+package com.fluteink.ssm.service;
+
+/**
+ * @author 明宇
+ * @version 1.0
+ */
+public interface EmployeeService {
+}
---
+++ ssm/src/main/java/com/fluteink/ssm/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,13 @@
+package com.fluteink.ssm.service.impl;
+
+import com.fluteink.ssm.service.EmployeeService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author 明宇
+ * @version 1.0
+ */
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+}
