d42f9a2abf04288435bfee0dbccd3844b773fe33
fluteink <fluteink@qq.com>
2022-10-30 17:34:01 +0800

    commit

--- ssm/src/main/java/com/fluteink/ssm/controller/EmployeeController.java
+++ ssm/src/main/java/com/fluteink/ssm/controller/EmployeeController.java
@@ -1,6 +1,12 @@
-package com.fluteink.ssm.controller;/**
- * @author  明宇
+package com.fluteink.ssm.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author 明宇
  * @version 1.0
- */public class EmployeeController {
-    
+ */
+@Controller
+public class EmployeeController {
+
 }
