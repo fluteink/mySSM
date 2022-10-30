a84b0079a8f4671a5027e9e1f1ffe92fb38c78e8
fluteink <fluteink@qq.com>
2022-10-30 21:46:03 +0800

    提交

--- ssm/src/main/java/com/fluteink/ssm/service/impl/EmployeeServiceImpl.java
+++ ssm/src/main/java/com/fluteink/ssm/service/impl/EmployeeServiceImpl.java
@@ -4,12 +4,14 @@ import com.fluteink.ssm.service.EmployeeService;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @author 明宇
  * @version 1.0
  */
 @Service
+@Transactional
 public class EmployeeServiceImpl implements EmployeeService {
 
 }
