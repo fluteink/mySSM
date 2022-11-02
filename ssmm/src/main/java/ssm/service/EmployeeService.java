fac14f89bb1eaabc41fe610525db7db6fa492833
fluteink <fluteink@qq.com>
2022-11-02 13:45:17 +0800

    commit

---
+++ ssmm/src/main/java/ssm/service/EmployeeService.java
@@ -0,0 +1,13 @@
+package com.fluteink.ssm.service;
+
+import com.fluteink.ssm.pojo.Employee;
+
+import java.util.List;
+
+/**
+ * @author 明宇
+ * @version 1.0
+ */
+public interface EmployeeService {
+    List<Employee> getAllEmployees();
+}
